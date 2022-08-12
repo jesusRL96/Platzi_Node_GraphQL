0bad42d6999ec2a8c4b0f1be6868286cad2f0b7f
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-08-12 16:19:22 -0500

    Construyendo una query simple con Apollo Server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,20 @@
+import {ApolloServer} from 'apollo-server'
+// query
+const typeDefs = `
+    type Query {
+        info: String!
+    }
+`
+// resolvers
+const resolvers = {
+    Query: {
+        info: () => `API Platzi Node Graphql`
+    }
+}
 
+// iniciar servidor
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+server.listen().then(({url}) => console.log(`server is running on ${url}`) )
